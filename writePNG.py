fed3a33f22f79ce583d3f1c8514869d6d3d672ee
LeslieK <leslieklein@comcast.net>
2013-09-30 20:33:34 -0400

    initial commit

---
+++ writePNG.py
@@ -0,0 +1,13 @@
+import png, itertools
+
+p = range(8)
+# convert to (R, G, B) tuples = [(True, True, False), (, , ), ..., (False, True, True)]
+prgb = map(lambda x: (bool(x&2), bool(x&4), bool(x&1)), p)
+# flatten
+# [(True, True, False), ...] => [True, True, False, ...]
+it = itertools.chain(*prgb)
+row = map((255).__mul__, it)
+w = png.Writer(width=8, height=1, bitdepth=1, greyscale=False)
+with open('myrgp.png', 'wb') as f:
+	w.write(f, [row])
+f.close()
